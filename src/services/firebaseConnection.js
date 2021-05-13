67242669f092565106a7fc0b8bdb4620058196af
Renato de Oliveira Rodrigues <r.oliveira.rodrigues@avanade.com>
2021-05-13 19:50:59 -0300

    projeto Pc Info

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+let firebaseConfig = {
+  apiKey: 'AIzaSyDwqGTwP2H9XX4gosOsiPVMrYVsNeVGvD0',
+  authDomain: 'sistema-df140.firebaseapp.com',
+  projectId: 'sistema-df140',
+  storageBucket: 'sistema-df140.appspot.com',
+  messagingSenderId: '14977229227',
+  appId: '1:14977229227:web:30278fc575a99b478a32ee',
+  measurementId: 'G-WT5VKSSZR9',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
